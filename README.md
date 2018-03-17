fb1b11bae0e8dcbde8391149cd952a51e6638783
pablovesga <pablovesga@outlook.com>
2018-03-16 21:51:14 -0500

    Version probada y rotulada

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # HerremientasDigitales1
 Tarea de herramientas digitales elaborada en processing
+
+Me gusto mucho processing, le voy a sacar mucho el juego.
+
+Pero, aprender github me ayudara demasiado de acá en adelante.
+
+
--- pinghtml/pinghtml_pde/pinghtml_pde.js
+++ pinghtml/pinghtml_pde/pinghtml_pde.js
@@ -1,83 +1,97 @@
-function setup() {
-int value=0;
-int x=0;
-int y=150;
-int xv=0;
-int yv=0;
-int z=1;
-int p=0;
-int[] bl = new int[10];
-int j=0;
-PImage foto;
-  size(1024,720);
-  PFont font;
-  font=loadFont("HanziPenTC-W5-24.vlw");
-  foto=loadImage("uno.jpg");
-  textFont(font,32);
-  for (int i =0; i<10; i=i+1){
-    bl[i]=1;
-  }
-  x = int(random(1020));
+  var value=0;
+  var x=0;    //posición en el eje x de la bola
+  var y=150;  //posición en el eje y de la bola
+  var xv=0;   //sentido hacia la izquierda=0 derecha=1
+  var yv=0;   //sentido hacia abajo=0 arriba=1
+  var z=-1;    //valor de dificultad
+  var p=0;    //variable que mantiene el puntaje
+  var bl = new Array(10);  //array para manejar los bloques
+  var j=0;    //Variable auxiliar
+  var foto;    //variable en que se guarda la imagen
+  var font1;   //variable en que se guarda la fuente
+  var i;      //Variable auxiliar
+  
 
+function setup() {
+  createCanvas(1024,620);                  //se inicializa tamaño, carga imagen y fuentes
+  foto = loadImage('imagen/uno.jpg');
+  font1=loadFont("imagen/HanziPenTC-W5-24.vlw");
 }
 
 function draw() {
-  background(250,250,250);
+  background(250,250,250);    //Carga fondo para borrar rastro anterior
   image(foto,200,150);
-  text("ping pong", 10, 20);
-  if(z==0){
+  textSize(32);
+  textFont(font1);
+  text("PING PONG", 10, 32);    //descripción titulo
+  if(z==0){                     //z=0 significa que el juego termino
     fill(0,255,0);
-    text("game over", 450, 300);
-  } else {
-    p=z;
+    textSize(32);
+    textFont(font1);
+    text("game over -Nuevo juego clic-", 310, 200);
   }
-  text(p,200,20);
+  if(z==-1){                    //mensaje con instrucción para iniciar juego solo aparece cuando se reinicia
+    fill(0,255,0);
+    textSize(32);
+    textFont(font1);
+    text("Para iniciar el juego haga clic", 300, 200);
+  }
+  fill(0,255,0);
+  text("Puntaje "+p,250,32);
   fill(100,0,100);
-  for(int i=0; i<10; i=i+1){
-    if(bl[i]==1){
+  for(i=0; i<10; i=i+1){    // for principal donde se ejecutan las condiciones del juego
+    if(bl[i]==1){           // se pintan los rectangulos habilitados
       rect((i*100+20), 100, 80, 20);
     }
-    if(y>100&&y<120){
+    if(y>100&&y<120){      //se pregunta si la bola está rompiendo algún bloque en eje y y x
     if(x>i*100+20&&x<i*100+100){
-      bl[i]=0;
-      if(yv==1){
-        yv=0;
-      } else {
-        yv=1;
+      if(bl[i]==1){        //si está habilitada ejecuta el cambio, de estár inhabilitada no hace nada
+        bl[i]=0;            //inhabilita bloque
+        z=z+1;              //aumenta dificultad con cada bloque roto
+        if(yv==1){          //cambia dirección por rotura de bloque
+          yv=0;
+        } else {
+          yv=1;
+        }
+        if(yv==0){
+          y=y+z;
+        } else {
+          y=y-z;
+        }
       }
     }
     }
   }
   
-  fill(0,0,0);
+  fill(0,0,0);              //dibuja bola en posición X y Y
   ellipse(x, y, 10, 10);
-  rect(mouseX,700,100,15);
-  if(x>1014){
+  rect(mouseX,600,100,15);  //Dibuja rectangulo que sirve de raqueta
+  if(x>1014){               //se establecen los limites en x o paredes
     xv=1;
   }
   if(x<10){
     xv=0;
   }
-  if(y>710){
+  if(y>610){                // si toca la parte de abajo pierde
     z=0;
   }
-  if(y<60){
+  if(y<60){                 //rebote superior
     yv=0;
   }
-  if(xv==0){
-    x=x+z;
+  if(xv==0){                //aumento de dirección en los ejes según dificultad
+    x=x+z/2;
   } else {
-    x=x-z;
+    x=x-z/2;
   }
   if(yv==0){
-    y=y+z;
+    y=y+z/2;
   } else {
-    y=y-z;
+    y=y-z/2;
   }
-  if(y>698&&y<710){
+  if(y>598&&y<610){        //condición de golpe de raqueta de existir se aumenta un punto
     if(x>mouseX&&x<mouseX+100){
     yv=1;
-    z=z+1;
+    p=p+1;
     }
   }
   
@@ -85,19 +99,13 @@ function draw() {
 
 
 
-function mouseClicked() {
-  if (value == 0) {
-    value = 255;
-  } else {
-    value = 0;
+function mouseClicked() {        //cuando se pulsa clic se reinician las variables para comenzar el juego
+  for (i = 0; i < 10; i += 1)
+  {
+    bl[i]=1;
   }
-  ellipse(mouseX, mouseY, mouseX/2, mouseY/2);
-  clear();
-}
-
-function mouseDragged() 
-{
-fill(50,100,30);
-stroke(205,155,225);
-ellipse(mouseX,mouseY, 5, 5);
+  x = int(random(1020));          //cada vez que reinicia la posición en x se genera aleatoria, para que cada juego sea diferente
+  p=0;
+  y=150;
+  z=2;
 }
